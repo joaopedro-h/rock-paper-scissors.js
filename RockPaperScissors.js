db1f6bddfc33ef3744d004ff5b70adc4eb03a0b4
João Pedro <joao.hermenegildocosta@gmail.com>
2026-03-30 22:56:55 -0300

    Adiciona placar ao jogo e novas estilizações no CSS

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,12 +1,17 @@
+/* Variáveis definidas fora da função para que a contagem não fosse zerada toda vez que fosse feito alguma jogada. */
+let vitorias = 0;
+let derrotas = 0;
+let empates = 0;
+
 function jogo(jogadaUsuario) {
     
-    opcoes = ["Pedra", "Papel", "Tesoura"];
+    let opcoes = ["Pedra", "Papel", "Tesoura"];
     let jogadaMaquina = opcoes[Math.floor(Math.random() * opcoes.length)];
-
     let resultado;
 
     if (jogadaMaquina === jogadaUsuario) {
         resultado = "Empate."
+        empates++;
 
     }else if (
         (jogadaUsuario === "Pedra" && jogadaMaquina === "Tesoura") ||
@@ -14,14 +19,19 @@ function jogo(jogadaUsuario) {
         (jogadaUsuario === "Papel" && jogadaMaquina === "Pedra") 
     ) {
         resultado = "Você ganhou!"
+        vitorias++;
 
     }else{
         resultado = "Você perdeu!"
+        derrotas++;
     }
     
     document.getElementById("jogadaUsuario").textContent = "Jogada do usuário: " + jogadaUsuario;
     document.getElementById("jogadaMaquina").textContent = "Jogada do computador: " + jogadaMaquina;
     document.getElementById("resultado").textContent = "Resultado: " + resultado;
+    document.getElementById("vitorias").textContent = "Vitórias: " + vitorias;
+    document.getElementById("derrotas").textContent = "Derrotas: " + derrotas;
+    document.getElementById("empates").textContent = "Empates: " + empates;
     
 }
 
@@ -32,3 +42,5 @@ const botaoTesoura = document.getElementById("tesoura");
 botaoPedra.addEventListener("click", () => jogo("Pedra"));
 botaoPapel.addEventListener("click", () => jogo("Papel"));
 botaoTesoura.addEventListener("click", () => jogo("Tesoura"));
+
+
